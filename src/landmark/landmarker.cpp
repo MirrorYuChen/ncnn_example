d2df47eecd7baab79eea86e23c5dd1e1d49a1b81
MirrorYuChen <49817428+MirrorYuChen@users.noreply.github.com>
2020-02-03 01:15:15 +0800

    Update landmarker.cpp

--- src/landmark/landmarker.cpp
+++ src/landmark/landmarker.cpp
@@ -1,5 +1,7 @@
 #include "landmarker.h"
+#include "zqlandmark/zq_landmarker.h"
 
+namespace mirror {
 Landmarker::Landmarker() {
 
 }
@@ -15,4 +17,10 @@ int Landmarker::LoadModel(const char * root_path) {
 int Landmarker::ExtractKeypoints(const cv::Mat & img_src,
 	const cv::Rect & face, std::vector<cv::Point2f>* keypoints) {
 	return 0;
+}
+
+Landmarker* ZQLandmarkerFactory::CreateLandmarker() {
+	return new ZQLandmarker();
+}
+
 }
